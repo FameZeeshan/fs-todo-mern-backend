9a70245c6e9f3f2d82a3d28b795906ad36f49ea9
Mohammed Zeeshan <64826609+FameZeeshan@users.noreply.github.com>
2023-07-24 22:06:04 +0530

    Update ToDoRoute.js

--- routes/ToDoRoute.js
+++ routes/ToDoRoute.js
@@ -1,5 +1,10 @@
+// Import the ToDoModel from the '../models/ToDoModel' file
 const ToDoModel = require("../models/ToDoModel");
+
+// Import the Router from the 'express' library
 const { Router } = require("express");
+
+// Import the functions (controller methods) from the '../controllers/ToDoController' file
 const {
   getToDo,
   saveToDo,
@@ -7,9 +12,22 @@ const {
   deleteToDo,
 } = require("../controllers/ToDoController");
 
+// Create a new instance of the Router
 const router = Router();
+
+// Define the routes and associate them with the respective controller methods
+
+// Route for handling the GET request to retrieve all ToDo items
 router.get("/", getToDo);
+
+// Route for handling the POST request to save a new ToDo item
 router.post("/save", saveToDo);
+
+// Route for handling the POST request to update an existing ToDo item
 router.post("/update", updateToDo);
+
+// Route for handling the POST request to delete an existing ToDo item
 router.post("/delete", deleteToDo);
+
+// Export the router to be used in the main application file (e.g., 'Server.js')
 module.exports = router;
